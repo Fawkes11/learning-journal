c543459704841aac13b5b1cb9320cf6ef4655192
Hessler Inampuez <hesslerinampuez@gmail.com>
2025-03-10 20:34:52 -0500

    Cambie un poco la presentacion, no mucho eso si

--- src/app/page.js
+++ src/app/page.js
@@ -14,15 +14,14 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-disc text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Go to “change day” to see a little more of my practices.{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
+              Work in progress
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
